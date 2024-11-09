05080d0b3b07642fc8c31da98827478d3d646f41
deboschr <debospencher@gmail.com>
2024-11-09 22:22:34 +0700

    inisialisasi

--- README.md
+++
@@ -1,1 +0,0 @@
-# JARRDIN_DWH
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const routes = require("./routes.js");
+
+const app = express();
+
+dotenv.config({ path: "./config/.env" });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// synchronizeSIPBANJA();
+
+app.use("/", routes);
+
+const port = 3001;
+app.listen(port, () => {
+	console.log(`>> Server is running on http://localhost:${port}`);
+});
