d25e2e12b7d36a6978a4cce2ed8a0249008110bc
deboschr <debospencher@gmail.com>
2025-01-27 01:09:02 +0700

    asa

--- utils/etl/etl.js
+++ utils/etl/etl.js
@@ -1,13 +1,16 @@
 const { extractData } = require("./extract");
 const { transformData } = require("./transform");
 const { loadData } = require("./load");
+const { connectToDatabase } = require("./connection");
 
 async function runETL(dataJob) {
+	// extract data
+	const extractedData = await extractData(sourceConn, sourceTable);
 
-   
-
-	const extractedData = await extractData(sourceTable);
+	// transform data
 	const transformedData = transformData(extractedData);
+
+	// load data
 	const loadedData = await loadData(transformedData, targetTable);
 
 	console.log("ETL process completed successfully.");
